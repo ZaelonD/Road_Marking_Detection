6215f46f3beae67f4369352b38cddf3e5eeda63d
Александр Дубровин <xah799@mail.ru>
2023-06-10 17:32:35 +0300

    Initial commit

--- src/Form.java
+++ src/Form.java
@@ -3,22 +3,23 @@ import java.awt.*;
 
 public class Form {
     private final JFrame frame;
+    private final JLabel label;
     private final int WIDTH = 960;
     private final int HEIGHT = 640;
+    private final JButton openButton = new JButton("Открыть изображение");
+    private final JButton discoverButton = new JButton("Обнаружить");
     private static final String title = "Обнаружение разметки";
 
     public Form() {
         this.frame = new JFrame(title);
+        this.label = new JLabel();
+        label.setIcon(new ImageIcon("resources/solidWhiteCurve.jpg"));
         frame.setSize(WIDTH, HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        JButton openButton = new JButton("Открыть изображение");
-        JButton discoverButton = new JButton("Обнаружить");
         frame.getContentPane().add(BorderLayout.NORTH, openButton);
         frame.getContentPane().add(BorderLayout.SOUTH, discoverButton);
-        JLabel label = new JLabel();
-        label.setIcon(new ImageIcon("resources/solidWhiteCurve.jpg"));
         frame.getContentPane().add(BorderLayout.CENTER, label);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
@@ -32,5 +33,13 @@ public class Form {
 
     public int getHEIGHT() {
         return HEIGHT;
+    }
+
+    public JButton getOpenButton() {
+        return openButton;
+    }
+
+    public JButton getDiscoverButton() {
+        return discoverButton;
     }
 }
